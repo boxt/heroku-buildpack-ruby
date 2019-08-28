fd94358afb62a68c99e384fcc8a8b022b33a85ed
Stuart <stuart.chinery@gmail.com>
2019-08-28 10:41:58 +0100

    Fixed node version

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -4,7 +4,7 @@ require 'json'
 
 class LanguagePack::Helpers::Nodebin
   def self.hardcoded_node_lts
-    version = '10.16.1'
+    version = '10.16.0'
     {
       'number' => version,
       'url' => "https://s3.amazonaws.com/heroku-nodebin/node/release/linux-x64/node-v#{version}-linux-x64.tar.gz"
